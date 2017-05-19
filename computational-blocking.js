eaee037cc6dd75dec89e17a9d7f3abb996382fff
Kelvin Lin <kelvin94@live.cn>
2017-05-19 09:37:26 -0700

    delete unecessary files

--- computational-blocking.js
+++
@@ -1,9 +0,0 @@
-console.log(1);
-
-
-require('./_fibonacci.js');//this function blocks the flow of the main process
-
-//spawn a child process to deal with heavy computational functions
-
-
-console.log(2);
--- readFileSync.js
+++
@@ -1,6 +0,0 @@
-var fs = require('fs'); //fs is a native node module
-console.log("going to get a file");
-var file = fs.readFileSync('readFileSync.js'); //readfileSync is a method in 'fs' module 
-console.log("got file");
-
-console.log("app contin...");
