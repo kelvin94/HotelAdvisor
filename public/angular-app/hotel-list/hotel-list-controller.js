0b3b9744dfaaf68443d3fabaaf7eb682b8eb1e35
Kelvin Lin <kelvin94@live.cn>
2017-05-19 11:24:56 -0700

    registeration is fixed and can correctly log in and out

--- public/angular-app/hotel-list/hotel-list-controller.js
+++ public/angular-app/hotel-list/hotel-list-controller.js
@@ -2,7 +2,7 @@ angular.module('meanhotel').controller('HotelsController', HotelsController);
 
 function HotelsController(hotelDataFactory) {
   var vm = this;
-  vm.title = 'MEAN Hotel App';
+  vm.title = 'HotelAdvisor App';
   hotelDataFactory.hotelList().then(function(response) {
     // console.log(response);
     vm.hotels = response.data;
